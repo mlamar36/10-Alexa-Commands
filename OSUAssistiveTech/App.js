ffb5cb86feadecf229c7ddc44aa2744c29920e8b
mlamar36 <lamar.36@buckeyemail.osu.edu>
2023-11-28 14:32:21 -0500

    added icon

--- OSUAssistiveTech/App.js
+++ OSUAssistiveTech/App.js
@@ -2,8 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { useState, useEffect } from 'react';
 import * as Speech from 'expo-speech';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 export default function App() {
   const [isActive, setIsActive] = useState(false);
@@ -36,8 +35,8 @@ export default function App() {
       <Text style={styles.font}>PRESS BUTTON TO HEAR A JOKE</Text>
       <TouchableOpacity onPress={handleButtonPress} style={styles.button}>
         {/* <Text style={styles.buttonText}>Tell me a Joke</Text> */}
-        {/* <Icon name="face-grin-tears" size={30} color="#900" /> */}
-        <FontAwesomeIcon icon="fa-solid fa-face-grin-tears" />
+        <Icon name="grin-squint-tears" size={150} color="#FFFFFF" />
+        {/* <FontAwesomeIcon icon="fa-solid fa-face-grin-tears" /> */}
       </TouchableOpacity>
     </View>
   );
